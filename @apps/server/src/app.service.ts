19a6e572fde8d17ed4b9b66b1ad53058e5b62506
sandheep45 <budtut143@gmail.com>
2023-11-01 12:13:01 +0530

    removed commented code from prisma-dev/vite.config.ts

--- @apps/server/src/app.service.ts
+++ @apps/server/src/app.service.ts
@@ -6,16 +6,6 @@ export class AppService {
   constructor(private prisma: PrismaService) {}
 
   async getHello(): Promise<string> {
-  const newUser = await this.prisma.user.create({
-    data: {
-      email: "test@test.com",
-      name: "Test",
-      role: "ADMIN",
-    }
-  })
-
-  console.log(newUser);
-
     return 'Hello World!';
   }
 }
