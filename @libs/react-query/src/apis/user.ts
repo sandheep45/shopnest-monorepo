5015f17f0c2bfd99c538d1e580814c1d33d7f46b
sandheep45 <budtut143@gmail.com>
2023-11-04 21:32:05 +0530

    added zod parsing in the react query api

--- @libs/react-query/src/apis/user.ts
+++ @libs/react-query/src/apis/user.ts
@@ -1,7 +1,8 @@
-import { User } from "@shopnest/prisma-dev";
+import { User, UserSchema } from "@shopnest/prisma-dev";
 import axiosInstance from "../axios-instance";
 
 export const user = async () => {
   const { data } = await axiosInstance.get<User[]>("/");
-  return data;
+  const users = UserSchema.array().parse(data);
+  return users;
 };
